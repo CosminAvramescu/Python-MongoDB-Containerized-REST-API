8c7b27bbab197670732dc33b38b75c79033c9580
CosminAvramescu <cosmin.avramescu11@gmail.com>
2023-12-11 02:33:18 +0200

    dockerfile, docker-compose, app.js

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, Response
 app = Flask(__name__)
 import pymongo
 from bson import json_util, ObjectId
+from datetime import datetime
 
 def getDB():
     myclient = pymongo.MongoClient(host='test_mongodb',
@@ -17,13 +18,13 @@ def add_country():
     payload = request.get_json(silent=True)
     if payload is None or ('nume' not in payload or 'lat' not in payload or 'lon' not in payload):
         # Error handling
-        return Response(status=400)
+        return jsonify({"error": "Incorrect payload!"}), 400
     
     mydb = getDB()["tema2"]
     mycol = mydb["country"]
     
     if mycol.find_one({"nume": payload['nume']}):
-        return Response(status=409)
+        return jsonify({"error": "Country name already exists!"}), 409
 
     result = mycol.insert_one(payload)
 
@@ -50,14 +51,14 @@ def update_countries(id):
     if id is None or payload is None or 'id' not in payload or 'nume' not in payload \
         or 'lat' not in payload or 'lon' not in payload:
         # Error handling
-        return Response(status=400)
+        return jsonify({"error": "Incorrect id or payload!"}), 400
 
     mydb = getDB()["tema2"]
     mycol = mydb["country"]
     
     if mycol.find_one({"_id": ObjectId(id)}) is None:
         # Error handling
-        return Response(status=404)
+        return jsonify({"error": "Country id not found!"}), 404
     
     query = { "_id": ObjectId(id) }
     payload["_id"] = ObjectId(payload.pop("id"))
@@ -65,105 +66,167 @@ def update_countries(id):
     
     mycol.update_one(query, new)
     
-    return jsonify({"id": id}), 200
+    return jsonify({"Successfully updated country with id": id}), 200
 
 @app.route("/api/countries/<string:id>", methods=['DELETE'])
 def delete_country(id):
     if id is None:
         # Error handling
-        return Response(status=400)
+        return jsonify({"error": "Incorrect id!"}), 400
 
     mydb = getDB()["tema2"]
     mycol = mydb["country"]
     
     if mycol.find_one({"_id": ObjectId(id)}) is None:
         # Error handling
-        return Response(status=404)
+        return jsonify({"error": "Country id not found!"}), 404
     
     query = { "_id": ObjectId(id) }
     mycol.delete_one(query)
     
-    return jsonify({"id": id}), 200
+    return jsonify({"Successfully deleted country with id": id}), 200
 
-# # CITIES
 
-# @app.route("/api/cities", methods=['POST'])
-# def add_city():
-#     payload = request.get_json(silent=True)
-#     if not payload or payload['name'] is None:
-#         # Error handling
-#         return Response(status=400)
+# CITIES
 
-#     print(payload)
-#     city_id = 0
+@app.route("/api/cities", methods=['POST'])
+def add_city():
+    payload = request.get_json(silent=True)
+    if payload is None or ('idTara' not in payload or 'nume' not in payload or \
+        'lat' not in payload or 'lon' not in payload):
+        # Error handling
+        return jsonify({"error": "Incorrect payload!"}), 400
     
-#     resp = Response(
-#         response=json.dumps(city_id), status=201,  mimetype="text/plain")
-#     resp.headers['Access-Control-Allow-Origin'] = '*'
-#     return resp
-
-# @app.route("/api/cities", methods=['GET'])
-# def get_cities():
-#     cities = []
+    mydb = getDB()["tema2"]
+    mycol = mydb["city"]
     
-#     resp = Response(
-#         response=json.dumps(cities), status=200,  mimetype="text/plain")
-#     resp.headers['Access-Control-Allow-Origin'] = '*'
-#     return resp
-
-# @app.route("/api/cities/country/<int:id_Tara>", methods=['GET'])
-# def get_cities_by_country_id(id_Tara):
-#     cities = []
+    mycountrycol = mydb["country"]
     
-#     resp = Response(
-#         response=json.dumps(cities), status=200,  mimetype="text/plain")
-#     resp.headers['Access-Control-Allow-Origin'] = '*'
-#     return resp
-
-# @app.route("/api/cities/<int:id>", methods=['PUT'])
-# def update_cities(id):
-#     payload = request.get_json(silent=True)
-#     if not payload or (payload['id'] is None or payload['idTara'] is None \
-#         or payload['lat'] is None or payload['lon']is None):
-#         # Error handling
-#         return Response(status=400)
-
-#     print(payload)
+    if len(str(payload["idTara"]))!=24:
+        return jsonify({"error": "Incorrect id!"}), 400
+    if mycountrycol.find_one({"_id": ObjectId(payload["idTara"])}) is None:
+        return jsonify({"error": "Country id does not exist!"}), 404
     
-#     country = 0
-#     resp = Response(
-#         response=json.dumps(country), status=200,  mimetype="text/plain")
-#     resp.headers['Access-Control-Allow-Origin'] = '*'
-#     return resp
+    if mycol.find_one({"nume": payload["nume"], "idTara": payload["idTara"]}):
+        return jsonify({"error": "Pair (city name, country id) already exists!"}), 409
 
-# @app.route("/api/cities/<int:id>", methods=['DELETE'])
-# def delete_city(id):
-#     if False:
-#         # Error handling
-#         return Response(status=400)
+    result = mycol.insert_one(payload)
 
-#     country = 0
-#     resp = Response(
-#         response=json.dumps(country), status=200,  mimetype="text/plain")
-#     resp.headers['Access-Control-Allow-Origin'] = '*'
-#     return resp
-
-# # TEMPERATURES
-
-# @app.route("/api/temperatures", methods=['POST'])
-# def add_temperature():
-#     payload = request.get_json(silent=True)
-#     if not payload or payload['id_oras'] is None:
-#         # Error handling
-#         return Response(status=400)
-
-#     print(payload)
-#     temperature_id = 0
+    response_data = {"id": str(result.inserted_id)}
     
-#     resp = Response(
-#         response=json.dumps(temperature_id), status=201,  mimetype="text/plain")
-#     resp.headers['Access-Control-Allow-Origin'] = '*'
-#     return resp
+    return jsonify(response_data), 201
+
+@app.route("/api/cities", methods=['GET'])
+def get_cities():
+    mydb = getDB()["tema2"]
+    mycol = mydb["city"]
+    
+    cities = []
+
+    for x in mycol.find():
+        x["id"] = str(x.pop("_id"))
+        cities.append(x)
+    
+    return jsonify(cities), 200
+
+@app.route("/api/cities/country/<string:id_Tara>", methods=['GET'])
+def get_cities_by_country_id(id_Tara):
+    mydb = getDB()["tema2"]
+    mycol = mydb["city"]
+    
+    cities = []
+
+    for x in mycol.find({"idTara": id_Tara}):
+        x["id"] = str(x.pop("_id"))
+        cities.append(x)
+    
+    return jsonify(cities), 200
+
+@app.route("/api/cities/<string:id>", methods=['PUT'])
+def update_cities(id):
+    payload = request.get_json(silent=True)
+    if id is None or payload is None or 'id' not in payload or 'idTara' not in payload \
+        or 'nume' not in payload or 'lat' not in payload or 'lon' not in payload:
+        # Error handling
+        return jsonify({"error": "Incorrect id or payload!"}), 400
+
+    mydb = getDB()["tema2"]
+    mycol = mydb["city"]
+    
+    if mycol.find_one({"_id": ObjectId(id)}) is None:
+        # Error handling
+        return jsonify({"error": "City id not found!"}), 404
+    
+    query = { "_id": ObjectId(id) }
+    payload["_id"] = ObjectId(payload.pop("id"))
+    new = { "$set": payload }
+    
+    if mycol.find_one({"nume": payload["nume"], "idTara": payload["idTara"]}):
+        return jsonify({"error": "Pair (city name, country id) already exists!"}), 409
+    
+    mycol.update_one(query, new)
+    
+    return jsonify({"Successfully updated city with id": id}), 200
+
+@app.route("/api/cities/<string:id>", methods=['DELETE'])
+def delete_city(id):
+    if id is None:
+        # Error handling
+        return jsonify({"error": "Incorrect id!"}), 400
+
+    mydb = getDB()["tema2"]
+    mycol = mydb["city"]
+    
+    if mycol.find_one({"_id": ObjectId(id)}) is None:
+        # Error handling
+        return jsonify({"error": "City id not found!"}), 404
+    
+    query = { "_id": ObjectId(id) }
+    mycol.delete_one(query)
+    
+    return jsonify({"Successfully deleted city with id": id}), 200
+
+# TEMPERATURES
+
+@app.route("/api/temperatures", methods=['POST'])
+def add_temperature():
+    payload = request.get_json(silent=True)
+    if payload is None or ('idOras' not in payload or 'valoare' not in payload):
+        # Error handling
+        return jsonify({"error": "Incorrect payload!"}), 400
+    
+    mydb = getDB()["tema2"]
+    mycol = mydb["temperatures"]
+    
+    mycitycol = mydb["city"]
+    
+    if len(str(payload["idOras"]))!=24:
+        return jsonify({"error": "Incorrect id!"}), 400
+    if mycitycol.find_one({"_id": ObjectId(payload["idOras"])}) is None:
+        return jsonify({"error": "City id does not exist!"}), 404
+    
+    payload['timestamp']=datetime.now()
+    if mycol.find_one({"idOras": payload["idOras"], "timestamp": payload["timestamp"]}):
+        return jsonify({"error": "Pair (city name, country id) already exists!"}), 409
+
+    result = mycol.insert_one(payload)
+
+    response_data = {"id": str(result.inserted_id)}
+    
+    return jsonify(response_data), 201
+
+@app.route("/api/temperatures", methods=['GET'])
+def get_temperatures():
+    mydb = getDB()["tema2"]
+    mycol = mydb["temperatures"]
+    
+    cities = []
+
+    for x in mycol.find():
+        x["id"] = str(x.pop("_id"))
+        cities.append(x)
+    
+    return jsonify(cities), 200
 
 # @app.route("/api/temperatures?lat=<double:lat>&lon=<double:lon>&from=<double:date>&until=<Date:until>", methods=['GET'])
 # def get_temperatures_by_location_or_timestamp():
